5d8268595e633ce59cea2a3975a1761f503f6b14
Kira Murzaneva <marccappy76@gmail.com>
2024-10-03 00:34:13 +0300

    add button

--- src/pages/news/NewsCard/NewsCard.jsx
+++ src/pages/news/NewsCard/NewsCard.jsx
@@ -9,11 +9,11 @@ const NewsCard = ({ title, description, image, createdAt }) => {
           <div className="card-image"></div> {/* Черный квадрат */}
           <p className="card-date">{createdAt}</p>
         </div>
-        <div className='dec'></div>
       <div className="card-content">
           <h2 className="card-title">{title}</h2>
           <p className="card-description">{description}</p>
        </div>
+        <div className='dec'></div>
      </div>
     );
 };
