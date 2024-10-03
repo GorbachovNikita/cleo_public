b71c4673afd00d0665cd48d557e89089b2b31d2e
Kira Murzaneva <marccappy76@gmail.com>
2024-10-03 12:52:21 +0300

    fix

--- src/pages/news/NewsCard/NewsCard.jsx
+++ src/pages/news/NewsCard/NewsCard.jsx
@@ -3,18 +3,18 @@ import './NewsCard.page.css'
 
 const NewsCard = ({ title, description, image, createdAt }) => {
     return (
-      <div className="card">
+      <article className="card">
         <div>
           {/* <img src={image} alt={title} className="card-image" /> */}
           <div className="card-image"></div> {/* Черный квадрат */}
-          <p className="card-date">{createdAt}</p>
+          <time className="card-date">{createdAt}</time>
         </div>
       <div className="card-content">
           <h2 className="card-title">{title}</h2>
           <p className="card-description">{description}</p>
        </div>
         <div className='dec'></div>
-     </div>
+     </article>
     );
 };
 
