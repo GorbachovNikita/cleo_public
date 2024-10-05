d2b870de7f92ed3e02e61c343b12c8941817b292
Kira Murzaneva <marccappy76@gmail.com>
2024-10-05 08:52:26 +0300

    add banner

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import CartBlock from './CartBlock/CardtBlock'
+import CartBlock from './CartBlock/CardtBlock';
 
 const Carts = () => {
     return (
