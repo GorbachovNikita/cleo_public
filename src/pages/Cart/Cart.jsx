3a84753a8cabb00044da84fc015e071a786f0e84
piierro <nikitagorbachovbar@yandex.ru>
2024-10-05 18:54:44 +0400

    the sixth commit

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import CartBlock from './CartBlock/CardtBlock';
+
+const Carts = () => {
+    return (
+        <>
+            <Header/>
+            <div className='container'>
+                <CartBlock/>
+            </div>
+        </>
+    );
+};
+
+export default Carts;
